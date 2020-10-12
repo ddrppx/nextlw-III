b99f61e96a91deece9988d95a7b4b0b37c112403
ddrppx <ddrppx@gmail.com>
2020-10-12 17:46:02 -0300

    css e imports

---
+++ web/src/routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/app" component={} />
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
