ce411b7fd259897b9b3ab9ebf8b4209baf55e2d0
ddrppx <ddrppx@gmail.com>
2020-10-13 18:53:12 -0300

    table images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -9,6 +9,15 @@ export default {
         const orphanages = await orphanagesRepository.find()
         return response.json(orphanages);
     },
+    
+    async show(request: Request, response: Response){
+        const { id } = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.findOneOrFail(id);
+        return response.json(orphanages);
+    },
 
     async create(request: Request, response: Response) {
         const {
