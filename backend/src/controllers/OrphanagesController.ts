fa6bd70fa525f42373fc92ac52ca5c11047bd3fb
ddrppx <ddrppx@gmail.com>
2020-10-14 13:05:11 -0300

    Yup and validations

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
 import { getRepository, Repository } from 'typeorm';
+
+import * as Yup from 'yup';
+
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_views';
+
+
 
 export default {
     async index(request: Request, response: Response){
@@ -42,7 +47,7 @@ export default {
             return { path: image.filename }
         });
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -51,7 +56,28 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
     
