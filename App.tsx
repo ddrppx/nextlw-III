65c237a180886e5c1aa51708a9c7aa7407da8896
ddrppx <ddrppx@gmail.com>
2020-10-15 15:48:01 -0300

    landing button and css

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
 
-import mapMarker from './src/images/mao-marker.png';
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
@@ -12,12 +13,39 @@ export default function App() {
         style={styles.map} 
         provider={PROVIDER_GOOGLE}
         initialRegion={{
-          latitude: -21.0302958,
-          longitude: -40.8256505,
+          latitude: -21.0302507,
+          longitude: -40.8159322,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }} 
-      />
+      >
+        <Marker 
+        icon={mapMarker} 
+        calloutAnchor={{
+          x: 2.7,
+          y: 0.8,
+        }}
+        coordinate={{
+          latitude: -21.0302507,
+          longitude: -40.8159322, 
+        }}>
+          <Callout tooltip onPress={() => {}} >
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+      
+        <TouchableOpacity 
+          style={styles.createOrphanageButton} 
+          onPress={() => {}}>
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -31,5 +59,51 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
 
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 46,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+
+  footerText: {
+    color: '#8fa7b3'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
