94204128ceaeec57a5017067b520ba957d27b7bc
ddrppx <ddrppx@gmail.com>
2020-10-16 15:17:54 -0300

    map points and details page

--- src/pages/OprhanageDetails.tsx
+++ src/pages/OprhanageDetails.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
 
 import mapMarkerImg from '../images/map-marker.png';
 import { RectButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native'
+import api from '../services/api';
 
-export default function OrphanageDetails() {
+interface OrphanageDetailsRouteParams {
+  id: number;
+}
+
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    id: number;
+    url: string;
+  }>;
+}
+
+export default function OrphanageDetails() {  
+  const route = useRoute();
+  const [orphanage, setOrphanage] = useState<Orphanage>();
+  const params = route.params as OrphanageDetailsRouteParams;
+
+  useEffect(() => {
+    api.get(`orphanages/${params.id}`). then(response => {
+      setOrphanage(response.data);
+    })
+  }, [params.id]);
+
+  if (!orphanage) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>Carregando...</Text>
+      </View>
+    );
+  }
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
@@ -18,7 +57,7 @@ export default function OrphanageDetails() {
       </View>
 
       <View style={styles.detailsContainer}>
-        <Text style={styles.title}>Orf. Esperança</Text>
+        <Text style={styles.title}>{orphanage.name}</Text>
         <Text style={styles.description}>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</Text>
       
         <View style={styles.mapContainer}>
